74ee7866cdf09ccd4b93533bf798fece6effdd11
BBQ <40503804+barbecue@users.noreply.github.com>
2021-02-20 11:12:43 +0300

    ♻️ Created Team page.

---
+++ pages/team.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="flex items-center justify-center text-center h-screen">
+      <div class="grid grid-cols-1 gap-4">
+        <h1 class="devcells-title animate__animated animate__backInDown">
+          TEAM
+        </h1>
+        <div style="font-family: whitney" class="projects-box">
+          <a
+            v-for="member in members"
+            v-bind:key="member.login"
+            :href="`https://github.com/${member.login}`"
+            ><div
+              class="animate__animated animate__jackInTheBox user-card h-15 flex items-center p-2 rounded-xl shadow"
+            >
+              <div class="ml-1 flex items-center space-x-4">
+                <img
+                  draggable="false"
+                  :src="member.avatar_url"
+                  class="w-12 h-12 rounded-full"
+                />
+              </div>
+              <div class="p-3">
+                <div class="font-semibold">
+                  {{ member.login }}
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flex justify-center items-center align-center mt-5 mr-2">
+          <a
+            href="/"
+            class="animate__animated animate__backInUp button rounded-full font-bold text-center text-white px-4 py-3 transition duration-300 ease-in-out"
+          >
+            Go Back
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      members: [],
+    };
+  },
+  created() {
+    axios({
+      url: "https://api.github.com/orgs/devcells/members",
+      method: "GET",
+    }).then((response) => {
+      this.members = response.data;
+    }); // If the api shuts down. My website shut downs sadly.
+  },
+};
+</script>
