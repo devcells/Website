4713c37defb1d87d17df4e01e9e48864f5fb693b
BBQ <40503804+barbecue@users.noreply.github.com>
2021-02-20 14:23:02 +0300

    👻 Added Discord link

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
       <div class="grid grid-cols-2 gap-4 text-center items-center">
         <a
           target="_blank"
-          href="https://discord.com/atMxBEG4ne"
+          href="https://discord.gg/atMxBEG4ne"
           class="animate__animated animate__backInLeft button rounded-full font-bold text-center text-white px-4 py-3 transition duration-300 ease-in-out"
         >
           DISCORD
