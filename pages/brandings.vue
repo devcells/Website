3a5d0cb0449d474f856eb2b1ad21b3e4f2a8ff6a
BBQ <40503804+barbecue@users.noreply.github.com>
2021-02-20 11:09:48 +0300

    📝 Formatted

--- pages/brandings.vue
+++ pages/brandings.vue
@@ -1,40 +1,60 @@
 <template>
   <div>
-    <div style="font-family:whitney;" class="flex items-center justify-center text-center">
+    <div
+      style="font-family: whitney"
+      class="flex items-center justify-center text-center"
+    >
       <div class="grid grid-cols-1 gap-4">
         <h1 class="devcells-title animate__animated animate__backInDown">
           Brandings
         </h1>
-          <h1 class="animate__animated animate__backInLeft">Our logo <p style="color: #b3b3b4;">( Please do not edit, change, distort, recolor, or reconfigure the DevCells logo. )</p></h1>
-        <div style="overflow:hidden !important;" class="projects-box">
+        <h1 class="animate__animated animate__backInLeft">
+          Our logo
+          <p style="color: #b3b3b4">
+            ( Please do not edit, change, distort, recolor, or reconfigure the
+            DevCells logo. )
+          </p>
+        </h1>
+        <div style="overflow: hidden !important" class="projects-box">
           <div class="brand-image animate__animated animate__backInLeft">
-            <img src="/images/DevCells_White.png">
+            <img src="/images/DevCells_White.png" />
           </div>
           <div class="brand-image animate__animated animate__backInUp">
-            <img src="/images/DevCells_Black.png">
+            <img src="/images/DevCells_Black.png" />
           </div>
           <div class="brand-image animate__animated animate__backInRight">
-            <img src="/images/DevCells_Gradient.png">
+            <img src="/images/DevCells_Gradient.png" />
           </div>
         </div>
         <h1 class="animate__animated animate__backInLeft">Logo + Wordmark</h1>
-        <div  style="overflow:hidden !important;" class="projects-box">
-          <div class="brand-image-wordmark flex items-center justify-center animate__animated animate__backInLeft">
-            <img src="/images/DevCells_White_Text.png">
+        <div style="overflow: hidden !important" class="projects-box">
+          <div
+            class="brand-image-wordmark flex items-center justify-center animate__animated animate__backInLeft"
+          >
+            <img src="/images/DevCells_White_Text.png" />
           </div>
-          <div class="brand-image-wordmark flex items-center justify-center animate__animated animate__backInUp">
-            <img src="/images/DevCells_Black_Text.png">
+          <div
+            class="brand-image-wordmark flex items-center justify-center animate__animated animate__backInUp"
+          >
+            <img src="/images/DevCells_Black_Text.png" />
           </div>
-          <div class="brand-image-wordmark flex items-center justify-center animate__animated animate__backInRight">
-            <img src="/images/DevCells_Gradient_Text.png">
+          <div
+            class="brand-image-wordmark flex items-center justify-center animate__animated animate__backInRight"
+          >
+            <img src="/images/DevCells_Gradient_Text.png" />
           </div>
         </div>
 
-        <div  style="overflow:hidden !important;" class="flex justify-center items-center align-center mt-5 mr-2 mb-6">
-          <a  href="/" class="animate__animated animate__backInUp button rounded-full font-bold text-center text-white px-4 py-3 transition duration-300 ease-in-out">
+        <div
+          style="overflow: hidden !important"
+          class="flex justify-center items-center align-center mt-5 mr-2 mb-6"
+        >
+          <a
+            href="/"
+            class="animate__animated animate__backInUp button rounded-full font-bold text-center text-white px-4 py-3 transition duration-300 ease-in-out"
+          >
             Go Back
           </a>
-        
         </div>
       </div>
     </div>
@@ -42,7 +62,9 @@
 </template>
 
 <style>
-  *,body,html {
-    overflow:auto
-  }
+*,
+body,
+html {
+  overflow: auto;
+}
 </style>
